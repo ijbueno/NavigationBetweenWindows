b6a38c997045f36e572850d2abf5a2a3b39f695b
Isaac J. Bueno <ijbueno.apks@gmail.com>
2021-01-10 13:50:51 -0300

    Codificar programação para efetuar as navegações entre as janelas no TerceiroFragment.kt

--- app/src/main/java/br/com/ijbuenoapks/navigationbetweenwindows/TerceiroFragment.kt
+++ app/src/main/java/br/com/ijbuenoapks/navigationbetweenwindows/TerceiroFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,13 @@ class TerceiroFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_terceiro, container, false)
+        //defino a variavel que gerenciara o fragmento
+        val view = inflater.inflate(R.layout.fragment_terceiro, container, false)
+        //capturo os botoes e defino as ações para o click deles levarem para as janelas de acordo com cada uma delas
+        view.findViewById<Button>(R.id.cmd5).setOnClickListener { Navigation.findNavController(view).navigate(R.id.navegarTerceiroParaPrimeiro) }
+        view.findViewById<Button>(R.id.cmd6).setOnClickListener { Navigation.findNavController(view).navigate(R.id.navegarTerceiroParaSegundo) }
+
+        return view
     }
 
     companion object {
